84d3ca6582006c66265a46900635c8ea3991ca1e
terza_ <daniloterzic98@gmail.com>
2021-01-25 02:41:57 +0100

    Added state to Register component

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
-=======
-import React from 'react';
->>>>>>> 8396444a7facf68d5c71123cd3105e089858deab
 import {
   MDBContainer,
   MDBRow,
@@ -14,6 +10,24 @@ import {
 } from 'mdbreact';
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email1: '',
+    email2: '',
+    password: '',
+  });
+  const { name, email1, email2, password } = formData;
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const toSend = { name, email1, password };
+    if (email1 !== email2) {
+      alert('Emails must be same');
+      return;
+    }
+    console.log(toSend);
+  };
   return (
     <div className='kontejner1'>
       <MDBContainer>
@@ -21,7 +35,7 @@ const Register = () => {
           <MDBCol md='6'>
             <MDBCard>
               <MDBCardBody>
-                <form>
+                <form onSubmit={onSubmit}>
                   <p className='h4 text-center py-4'>Sign up</p>
                   <div className='grey-text'>
                     <MDBInput
@@ -29,34 +43,40 @@ const Register = () => {
                       icon='user'
                       group
                       type='text'
-                      validate
-                      error='wrong'
-                      success='right'
+                      name='name'
+                      value={name}
+                      onChange={onChange}
                     />
                     <MDBInput
                       label='Your email'
                       icon='envelope'
                       group
                       type='email'
-                      validate
                       error='wrong'
                       success='right'
+                      name='email1'
+                      value={email1}
+                      onChange={onChange}
                     />
                     <MDBInput
                       label='Confirm your email'
                       icon='exclamation-triangle'
                       group
                       type='text'
-                      validate
                       error='wrong'
                       success='right'
+                      name='email2'
+                      value={email2}
+                      onChange={onChange}
                     />
                     <MDBInput
                       label='Your password'
                       icon='lock'
                       group
                       type='password'
-                      validate
+                      name='password'
+                      value={password}
+                      onChange={onChange}
                     />
                   </div>
                   <div className='text-center py-4 mt-3'>

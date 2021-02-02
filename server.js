42a6e7e1f70ed5b26c12d4228a1aa414a4308728
terza_ <daniloterzic98@gmail.com>
2021-02-02 10:25:37 +0100

    Izmena

--- server.js
+++ server.js
@@ -1,33 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Ucitava mi variable iz config fajla...
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-// Inicijalni Middleware
-app.use(express.json());
-
-// Definisanje ruta
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/Shopping', {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    });
-
-    console.log('MongoDB Connected...');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-};
-
-// Povezivanje na bazu
-connectDB();
-
+// Sa process.env pristupamo config.env fajlu pa navodimo ime variable kojoj zelimo pristup
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+app.listen(PORT, console.log(`Server is listening on port ${PORT}`));
